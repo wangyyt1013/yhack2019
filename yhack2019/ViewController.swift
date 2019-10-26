3be1e5f4642771f93d493dc1b54d44fabee216d0
wangyyt1013 <yw3241@columbia.edu>
2019-10-26 12:04:56 -0400

    Initial Commit

---
+++ yhack2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yhack2019
+//
+//  Created by Yuanyuting Wang on 10/26/19.
+//  Copyright © 2019 rawe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
